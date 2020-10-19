8e51cfc19e17a7a8f08f8b5110d27c9e068036dd
ArturPiskarevS <71814718+ArturPiskarevS@users.noreply.github.com>
2020-10-19 19:35:22 +0300

    Turn buttons and message in gui

--- Coctails/Coctail/Assets/Scripts/ObiMixing.cs
+++ Coctails/Coctail/Assets/Scripts/ObiMixing.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Obi;
+using TMPro;
 using UnityEngine;
 
 public class ObiMixing : MonoBehaviour
@@ -8,12 +9,14 @@ public class ObiMixing : MonoBehaviour
     [SerializeField]
     private ObiSolver obiSolver;
 
+    [SerializeField] private ObiFluidRenderer renderer;
     [SerializeField] private ObiEmitter emitter1;
     [SerializeField] private ObiEmitter emitter2;
     [SerializeField] private ObiEmitter emitter3;
     [SerializeField] private ObiEmitter emitter4;
     [SerializeField] private ObiEmitter emitter5;
 
+    [SerializeField] private TextMeshProUGUI text;
     void Awake()
     {
         if (emitter1) emitter1.speed = 0f;
@@ -21,7 +24,10 @@ public class ObiMixing : MonoBehaviour
         if (emitter3) emitter3.speed = 0f;
         if (emitter4) emitter4.speed = 0f;
         if (emitter5) emitter5.speed = 0f;
-        
+        if (renderer == null)
+        {
+            renderer = FindObjectOfType<ObiFluidRenderer>();
+        }
     }
 
     public void StartEmmitterOne()
@@ -43,5 +49,31 @@ public class ObiMixing : MonoBehaviour
     public void StartEmmitterFive()
     {
         emitter5.speed = 3f;
+    }
+
+    public void TurnSurface()
+    {
+        renderer.settings.generateSurface = !renderer.settings.generateSurface;
+        text.text = "Surface Reconstruction : " + renderer.settings.generateSurface;
+    }
+    public void TurnLighting()
+    {
+        renderer.settings.lighting = !renderer.settings.lighting;
+        text.text = "Lighting : " + renderer.settings.lighting;
+    }
+    public void TurnReflection()
+    {
+        renderer.settings.generateReflection = !renderer.settings.generateReflection;
+        text.text = "Reflection : " + renderer.settings.generateReflection;
+    }
+    public void TurnRefraction()
+    {
+        renderer.settings.generateRefraction = !renderer.settings.generateRefraction;
+        text.text = "Refraction : " + renderer.settings.generateRefraction;
+    }
+    public void TurnFoam()
+    {
+        renderer.settings.generateFoam = !renderer.settings.generateFoam;
+        text.text = "Foam : " + renderer.settings.generateFoam;
     }
 }
